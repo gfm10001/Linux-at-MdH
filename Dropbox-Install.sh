fc28ceecd486f1b02ffd3f4bbd9d7fdc4f2b0d90
Göran Forsström <gfm10001@CZC4433ZFD.guest.mdh.se>
2017-02-14 19:08:05 +0100

    New dropbox id reminder

--- Dropbox-Install.sh
+++ Dropbox-Install.sh
@@ -1,16 +1,20 @@
 ##!/bin/bash
-
-echo
+echo ""
 echo "Install of Dropbox on MdH PC v.2017-02-10a/GF"
-echo
+echo ""
 echo "1. Download..."
 read -p "...starts at ENTER! Admin pw expected"
 sudo apt-get install nautilus-dropbox
-echo
+echo "(if fails try the command "
+echo 'sudo apt-get install nautilus-dropbox' 
+echo " directly!)"
+echo ""
 echo "2. Dropbox..."
 read -p "...starts at ENTER! Dropbox Login Window will be displayed!"
 dropbox start
-echo
+echo "(if fails try the command "
+echo 'dropbox start' 
+echo " directly!)"
+echo ""
 read -p "Finishes at ENTER "
-
 
